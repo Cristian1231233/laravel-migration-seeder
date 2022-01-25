87fbafc18231d77ead369751bbf1112966a3e6d2
Cristian1231233 <campagnacristian22@gmail.com>
2022-01-25 17:24:26 +0100

    init commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,10 @@
+<header>
+
+    <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="">Contatti</a></li>
+        <li><a href="">Altro</a></li>
+    </ul>
+
+</header>
